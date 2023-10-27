e3bcefebb53eac663cb5b1930cab075695c7e2f8
asifaziz969 <asif969aziz@gmail.com>
2023-10-27 19:36:13 +0500

    adding multiple documents

--- app.js
+++ app.js
@@ -13,13 +13,40 @@ const Fruit = mongoose.model("Fruit",fruitSchema );
 
 const fruit = new Fruit({  
 name: "apple",
-ratig:7,
+rating:7,
 review:"pretty solid as fruit"
  });
 
 
 fruit.save();
 
+const orange= new Fruit({
+    name: "orange",
+    rating: 5,
+    review:"it tastes good"
+
+})
+const banana= new Fruit({
+    name: "banana",
+    rating: 3,
+    review:"it tastes bad"
+
+})
+const kivi= new Fruit({
+    name: "kivi",
+    rating: 1,
+    review:"it tastes very bad"
+
+})
+
+const defaultItem= [orange,banana,kivi];
+
+Fruit.insertMany(defaultItem,{
+    if(err){
+      console.log(err);
+    }
+ 
+  });
 
 
 const personSchema =new mongoose.Schema({  
